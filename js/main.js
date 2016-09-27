566f2e1dd10c23d525ba7fdee404ea2c72622d17
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-09-27 11:24:35 +0200

    Add proto file.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 require(["protobuf"], function(ProtoBuf) {
   console.log("Hello.");
+
+  ProtoBuf.loadProtoFile("/proto/antidote.proto", function(err, builder) {
+    console.log("Protocol buffer file loaded.")
+  });
 });
